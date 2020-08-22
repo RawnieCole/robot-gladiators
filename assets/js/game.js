1622e6b72d88b84311332430d4323ecec623f3c5
RawnieCole <65691699+RawnieCole@users.noreply.github.com>
2020-08-22 02:14:23 -0500

    use JavaScript Functions to Communicate with the Player

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,29 @@
-window.alert( "This is an Alert! JavaScript is Running!" );
+var playerName = window.prompt( "What is Your Robot's Name?" );
+
+// What is This?
+console.log( playerName );
+
+console.log( "This Logs a String, Good for Leaving Yourself a Message" );
+
+// This Will Do Math and Log 20
+console.log( 10 + 10 );
+
+//What is This?
+console.log( "Our Robot's Name Is " + playerName );
+
+var playerName = "Tony The Robot";
+
+//Tony The Robot is Ready for Battle!
+console.log( "Tony The Robot" + " is Ready for Battle!" );
+
+//"Tony The Robot is Ready for Battle!"
+console.log( playerName + " is Ready for Battle!" );
+
+//"Your Robot, Tony The Robot, Has Won!
+console.log( "Your Robot, " + playerName + ", Has Won!" );
+
+// This Creates a Function Named "Fight"
+function fight() {
+    window.alert( "The Fight Has Begun!" );
+}
+// fight();
